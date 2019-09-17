08395f9e9155cb343b95cd74ca8987b2ba9d5a67
Jeffrey Paulraj <43931770+jeffreypaulraj@users.noreply.github.com>
2019-09-17 14:19:01 -0400

    Update and rename MainActivity.java to MainActivityColorSwap.java

--- MainActivityColorSwap.java
+++ MainActivityColorSwap.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivityColorSwap extends AppCompatActivity {
 
     Button colorButton;
     Button notColorButton;
