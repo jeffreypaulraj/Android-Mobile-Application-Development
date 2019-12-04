2889d01bfdbc75e2cbe77674569fec5d59d241ca
Jeffrey Paulraj <jeffreypaulraj@users.noreply.github.com>
2019-12-04 14:22:32 -0500

    listView proj inclass 2

--- ListViewProject/app/src/main/java/com/example/listviewproject/MainActivity.java
+++ ListViewProject/app/src/main/java/com/example/listviewproject/MainActivity.java
@@ -24,18 +24,32 @@ public class MainActivity extends AppCompatActivity {
 
     ListView teamListView;
     ArrayList<Player> playerList;
+    TextView playerPositionText;
+    TextView playerPointsText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         teamListView = findViewById(R.id.teamListView);
+        playerPositionText = findViewById(R.id.id_playerPositionText);
+        playerPointsText = findViewById(R.id.id_playerPointsText);
         playerList = new ArrayList<>();
-        playerList.add(new Player("Kawhi Leonard", "SF/PF", 25.9, R.drawable.kawhi));
+        playerList.add(new Player("Kawhi Leonard", "SF", 25.9, R.drawable.kawhi));
         playerList.add(new Player("Giannis Antetokounmpo", "PF", 30.8, R.drawable.giannis));
+        playerList.add(new Player("James Harden", "PF",39.5, R.drawable.harden));
+        playerList.add(new Player("Luka Doncic", "SF",33.8, R.drawable.luka));
+        playerList.add(new Player("Lebron James", "SF",25.6, R.drawable.lebron));
+        playerList.add(new Player("Jimmy Butler", "SF",19.0, R.drawable.jimmy));
+        playerList.add(new Player("Anthony Davis", "PF",26.1, R.drawable.anthony));
+        playerList.add(new Player("Pascal Siakam", "PF",25.1, R.drawable.pascal));
+        playerList.add(new Player("Damian Lillard", "PG",26.7, R.drawable.damian));
+        playerList.add(new Player("Paul George", "SF",23.5, R.drawable.paul));
+        playerList.add(new Player("Karl-Anthony Towns", "C",25.9, R.drawable.karl));
         CustomAdapter customAdapter = new CustomAdapter(this, R.layout.adapter_custom, playerList);
          teamListView.setAdapter(customAdapter);
 
     }
+
 
     public class Player{
 
@@ -76,9 +90,12 @@ public class MainActivity extends AppCompatActivity {
             xmlResource = resource;
         }
 
+        public void removeMethod() {
+            this.notifyDataSetChanged();
+        }
         @NonNull
         @Override
-        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
             LayoutInflater layoutInflater = (LayoutInflater) parentContext.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
             View view = layoutInflater.inflate(R.layout.adapter_custom, null);
 
@@ -87,6 +104,27 @@ public class MainActivity extends AppCompatActivity {
             image.setImageResource(playerList.get(position).getImage());
             Button nameButton = view.findViewById(R.id.id_adapter_nameButton);
             nameButton.setText("" + playerList.get(position).getName());
+            Button removeButton = view.findViewById(R.id.id_adapter_removeButton);
+
+            nameButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    playerPositionText.setText("Position: " + playerList.get(position).getPosition());
+                    playerPointsText.setText("Points Per Game: " + playerList.get(position).getPoints());
+                    for(int i = 0; i < playerList.size();i++){
+
+                    }
+                }
+            });
+
+            removeButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    playerList.remove(position);
+                    removeMethod();
+                }
+            });
+
             return view;
         }
 
